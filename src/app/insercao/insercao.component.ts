cb2ca2d043d3b2a2cb9b451f46042686afac6358
Douglas R V <dougviolante@gmail.com>
2020-11-30 17:35:07 -0300

    Componentes e Interface Cidade

---
+++ src/app/insercao/insercao.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-insercao',
+  templateUrl: './insercao.component.html',
+  styleUrls: ['./insercao.component.css']
+})
+export class InsercaoComponent
+{
+
+  constructor( private router        : Router ) { }
+
+
+
+}
