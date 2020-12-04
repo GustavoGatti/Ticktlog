d962453c61bc9ed918af29f8000b7d2f93cdba02
GustavoGatti <70723720+GustavoGatti@users.noreply.github.com>
2020-12-04 10:25:01 -0300

    Popup

--- src/shared/-beapicomm-service.service.ts
+++ src/shared/-beapicomm-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { take } from 'rxjs/operators';
-import {ICidade} from '../app/interfaces/cidade';
+import { ICidade } from '../app/interfaces/cidade';
 
 
 
@@ -9,32 +9,27 @@ import {ICidade} from '../app/interfaces/cidade';
   providedIn: 'root'
 })
 
-export class BEAPICommService 
-{
+export class BEAPICommService {
 
-  private readonly API_PostURL: string  = "https://ticketlog-tcs.herokuapp.com/api/cidades/estado=";
-  private PostURL:              string  = '';
-  
-  private novaCidade:           ICidade = {nome: '', populacao: 0};
-  public errorCount:           any    = 0;
-  public cidadesExistentes:    string[]  = [];
+  private readonly API_PostURL: string = "https://ticketlog-tcs.herokuapp.com/api/cidades/estado=";
+  private PostURL: string = '';
+
+  private novaCidade: ICidade = { nome: '', populacao: 0 };
+  public errorCount: any = 0;
+  public cidadesExistentes: string[] = [];
   private ok: number = 2;
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) { }
 
-  public IdentificaEstado(cidade:string) : number
-  {
+  public IdentificaEstado(cidade: string): number {
 
-    if(cidade == "Rio Grande do Sul")
-    {
+    if (cidade == "Rio Grande do Sul") {
       return 1;
     }
-    else if(cidade == "Santa Catarina")
-    {
+    else if (cidade == "Santa Catarina") {
       return 11;
     }
-    else if(cidade == "Paraná")
-    {
+    else if (cidade == "Paraná") {
       return 21;
     }
 
@@ -43,42 +38,41 @@ export class BEAPICommService
   }
 
 
-  public Post_NovaCidade(novaCidade: string, populacao: any, estado: string) : any
-  {
-    
+  public Post_NovaCidade(novaCidade: string, populacao: any, estado: string): any {
+
     let idEstado = this.IdentificaEstado(estado);
 
     this.PostURL = this.API_PostURL + idEstado.toString() + '/';
 
-    populacao    = Number(populacao);
+    populacao = Number(populacao);
 
     this.novaCidade = { nome: novaCidade, populacao: populacao };
 
-    this.http.post(this.PostURL, this.novaCidade).pipe(take(1)).subscribe(success => {this.ok = 1}, 
-                                                                  error   => {this.ok = 0});
+    this.http.post(this.PostURL, this.novaCidade).pipe(take(1)).subscribe(success => { this.ok = 1 },
+      error => { this.ok = 0 });
   }
 
 
-  public Post_LotesCidades(cidades: ICidade[]) : void
-  {
+  public Post_LotesCidades(cidades: ICidade[]): void {
     console.log(cidades)
 
     this.errorCount = 0;
 
-    for(let k = 0; k <= cidades.length; k++)
-    {
+    for (let k = 0; k <= cidades.length; k++) {
       let idEstado = this.IdentificaEstado(cidades[k].estado as string);
 
       this.PostURL = this.API_PostURL + idEstado.toString() + '/';
 
-      let cidade: ICidade = {nome: cidades[k].nome, populacao: cidades[k].populacao}
+      let cidade: ICidade = { nome: cidades[k].nome, populacao: cidades[k].populacao }
 
-      this.http.post(this.PostURL, cidade).pipe(take(1)).subscribe(success => {}, 
-                                                                   error   => {this.errorCount++, 
-    
-                                                                    this.cidadesExistentes.push(cidade.nome)});
+
+      this.http.post(this.PostURL, cidade).pipe(take(1)).subscribe(success => { },
+        error => {
+          this.errorCount++,
+          this.cidadesExistentes.push("Cidade: " + cidade.nome + " na linha " + (k + 1) + " da planilha")
+        });
       this.cidadesExistentes = [];
-      }
+    }
 
     //this.errorCount = -1;
   }
